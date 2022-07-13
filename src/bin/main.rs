b76df2c00019d866787046b131d52e262c2f6227
nickmachnik <nick.machnik@gmail.com>
2022-07-13 13:55:47 +0200

    impl hmc single state test run

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,13 @@
+use arrayfire::{dim4, randn, Array};
+use log::info;
 use ndarray::arr1;
+use rs_bann::afnet::{Arm, ArmBuilder};
 use rs_bann::network::MarkerGroup;
 use rs_bedvec::io::BedReader;
 
 fn main() {
-    test_crate();
+    simple_logger::init_with_level(log::Level::Info).unwrap();
+    test_crate_af();
 }
 
 // TODO:
@@ -28,7 +32,47 @@ fn predict() {
     unimplemented!();
 }
 
-fn test_crate() {
+fn test_crate_af() {
+    info!("Starting af test");
+
+    // make random data
+    let num_individuals: u64 = 100;
+    let num_markers: u64 = 10;
+    let hidden_layer_width: u64 = 2;
+    let x_train: Array<f64> = randn(dim4![num_individuals, num_markers, 1, 1]);
+    let w0: Array<f64> = randn(dim4![num_markers, hidden_layer_width, 1, 1]);
+    let w1: Array<f64> = randn(dim4![hidden_layer_width, 1, 1, 1]);
+    let w2: Array<f64> = randn(dim4![1, 1, 1, 1]);
+    let b0: Array<f64> = randn(dim4![1, hidden_layer_width, 1, 1]);
+    let b1: Array<f64> = randn(dim4![1, 1, 1, 1]);
+    let true_net = ArmBuilder::new()
+        .with_num_markers(num_markers as usize)
+        .add_hidden_layer(hidden_layer_width as usize)
+        .add_layer_weights(&w0)
+        .add_layer_biases(&b0)
+        .add_summary_weights(&w1)
+        .add_summary_bias(&b1)
+        .add_output_weight(&w2)
+        .verbose()
+        .build();
+    let y_train = true_net.predict(&x_train);
+
+    let mut train_net = ArmBuilder::new()
+        .with_num_markers(num_markers as usize)
+        .add_hidden_layer(hidden_layer_width as usize)
+        .with_initial_weights_value(1.)
+        .with_initial_bias_value(1.)
+        .verbose()
+        .build();
+
+    // train
+    let integration_length = 100;
+    let step_size = 0.001;
+
+    train_net.hmc_step(&x_train, &y_train, integration_length, step_size);
+}
+
+fn test_crate_ndarray() {
     let reader = BedReader::new("resources/test/four_by_two.bed", 4, 2);
     let mut mg = MarkerGroup::new(
         arr1(&[-0.587_430_3, 0.020_813_8, 0.346_810_51, 0.283_149_64]),
