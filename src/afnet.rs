48dbcb3c5a4cbf812eb000d1d9b58b449e141dda
nickmachnik <nick.machnik@gmail.com>
2022-07-26 17:06:44 +0200

    create StepSizes struct with dbg impl

--- src/afnet.rs
+++ src/afnet.rs
@@ -53,6 +53,48 @@ impl ArmMomenta {
     }
 }
 
+#[derive(Clone)]
+struct StepSizes {
+    wrt_weights: Vec<Array<f64>>,
+    wrt_biases: Vec<Array<f64>>,
+}
+
+impl fmt::Debug for StepSizes {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.param_vec())
+    }
+}
+
+impl StepSizes {
+    fn param_vec(&self) -> Vec<f64> {
+        let mut host_vec = Vec::new();
+        host_vec.resize(self.num_params(), 0.);
+        let mut insert_ix: usize = 0;
+        for i in 0..self.wrt_weights.len() {
+            let len = self.wrt_weights[i].elements();
+            self.wrt_weights[i].host(&mut host_vec[insert_ix..insert_ix + len]);
+            insert_ix += len;
+        }
+        for i in 0..self.wrt_biases.len() {
+            let len = self.wrt_biases[i].elements();
+            self.wrt_biases[i].host(&mut host_vec[insert_ix..insert_ix + len]);
+            insert_ix += len;
+        }
+        host_vec
+    }
+
+    fn num_params(&self) -> usize {
+        let mut res: usize = 0;
+        for i in 0..self.wrt_weights.len() {
+            res += self.wrt_weights[i].elements();
+        }
+        for i in 0..self.wrt_biases.len() {
+            res += self.wrt_biases[i].elements();
+        }
+        res
+    }
+}
+
 /// Weights and biases
 #[derive(Clone)]
 struct ArmParams {
@@ -164,8 +206,9 @@ impl Arm {
         step_size: f64,
     ) -> bool {
         let init_params = self.params.clone();
-        let init_rss = self.rss(x_train, y_train);
         // TODO: add heuristic step sizes
+        // for that I will need to keep last rounds gradient, step sizes and momenta
+
         // TODO: add u turn diagnostic for tuning
         let init_momenta = self.sample_momenta();
         let init_neg_hamiltonian = self.neg_hamiltonian(&init_momenta, x_train, y_train);
