4d65cb5c01474499353893ccf073cc6bd0149361
nickmachnik <nick.machnik@gmail.com>
2022-08-01 15:36:15 +0200

    impl predict and rss fns

--- src/net/net.rs
+++ src/net/net.rs
@@ -2,9 +2,11 @@ use super::branch::branch::Branch;
 use super::branch::branch::BranchCfg;
 use super::gibbs_steps::multi_param_precision_posterior;
 use super::gibbs_steps::single_param_precision_posterior;
+use super::mcmc_cfg::MCMCCfg;
 use crate::to_host;
 use arrayfire::sum_all;
-use arrayfire::{dim4, Array};
+use arrayfire::{dim4, Array, MatProp};
+use log::info;
 use rand::prelude::SliceRandom;
 use rand::rngs::ThreadRng;
 use rand::thread_rng;
@@ -53,13 +55,13 @@ impl Net {
     // X has to be column major!
     // TODO: X will likely have to be in compressed format on host memory, so Ill have to unpack
     // it before loading it into device memory
-    pub fn train(&mut self, x_train: &Vec<Vec<f64>>, y_train: &Vec<f64>, chain_length: usize) {
+    pub fn train(&mut self, x_train: &Vec<Vec<f64>>, y_train: &Vec<f64>, mcmc_cfg: &MCMCCfg) {
         let mut acceptance_counts: Vec<usize> = vec![0; self.num_branches];
         let mut rng = thread_rng();
         let num_individuals = y_train.len();
         let mut residual = Array::new(y_train, dim4![num_individuals as u64, 1, 1, 1]);
         let mut branch_ixs = (0..self.num_branches).collect::<Vec<usize>>();
-        for _ in 0..chain_length {
+        for _ in 0..mcmc_cfg.chain_length {
             // sample ouput bias term
             residual += self.output_bias.af_bias();
             self.output_bias.sample_bias(
@@ -90,7 +92,7 @@ impl Net {
                 // remove prev contribution from residual
                 residual += branch.predict(&x);
                 // TODO: use some input cfg for hmc params
-                if branch.hmc_step(&x, &residual, 70, None, 1000.) {
+                if branch.hmc_step(&x, &residual, &mcmc_cfg) {
                     acceptance_counts[branch_ix] += 1;
                 }
                 branch.sample_precisions(self.precision_prior_shape, self.precision_prior_scale);
@@ -107,6 +109,11 @@ impl Net {
                 &mut rng,
             );
         }
+        let acceptance_rates: Vec<f64> = acceptance_counts
+            .iter()
+            .map(|e| *e as f64 / mcmc_cfg.chain_length as f64)
+            .collect();
+        info!("acceptace rates per branch: {:?}", acceptance_rates);
     }
 
     // TODO: predict using posterior predictive distribution instead of point estimate
@@ -128,5 +135,34 @@ impl Net {
             y_hat += Branch::from_cfg(&cfg).predict(&x);
         }
         to_host(&y_hat)
+    }
+
+    // TODO: predict using posterior predictive distribution instead of point estimate
+    fn predict_arr(&self, x_test: &Vec<Vec<f64>>, num_individuals: usize) -> Array<f64> {
+        // I expect X to be column major
+        let mut y_hat = Array::new(
+            &vec![0.0; num_individuals],
+            dim4![num_individuals as u64, 1, 1, 1],
+        );
+        // add bias
+        y_hat += self.output_bias.af_bias();
+        // add all branch predictions
+        for branch_ix in 0..self.num_branches {
+            let cfg = &self.branch_cfgs[branch_ix];
+            let x = Array::new(
+                &x_test[branch_ix],
+                dim4!(num_individuals as u64, cfg.num_markers as u64),
+            );
+            y_hat += Branch::from_cfg(&cfg).predict(&x);
+        }
+        y_hat
+    }
+
+    pub fn rss(&self, x_test: &Vec<Vec<f64>>, y_test: &Vec<f64>) -> f64 {
+        let y_test_arr = Array::new(y_test, dim4!(y_test.len() as u64, 1, 1, 1));
+        let y_hat = self.predict_arr(&x_test, y_test.len());
+        let mut sum_of_squares = vec![0.0];
+        arrayfire::dot(&y_test_arr, &y_hat, MatProp::NONE, MatProp::NONE).host(&mut sum_of_squares);
+        sum_of_squares[0]
     }
 }
