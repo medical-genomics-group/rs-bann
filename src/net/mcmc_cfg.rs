fc49b195192e360040da5747c7662b8dadb8cf9c
nickmachnik <nick.machnik@gmail.com>
2022-08-15 12:31:20 +0200

    add trace_file to mcmc_cfg

--- src/net/mcmc_cfg.rs
+++ src/net/mcmc_cfg.rs
@@ -5,6 +5,7 @@ pub struct MCMCCfg {
     pub hmc_integration_length: usize,
     pub hmc_step_size_mode: StepSizeMode,
     pub chain_length: usize,
+    pub trace_file: Option<String>,
 }
 
 pub enum StepSizeMode {
