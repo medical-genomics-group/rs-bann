35ece7ef2786b65757425fabdcb1e2dccd75c70c
nickmachnik <nick.machnik@gmail.com>
2022-12-21 11:06:55 +0100

    add control over branch type to script

--- scripts/sim_train_pred.sh
+++ scripts/sim_train_pred.sh
@@ -2,6 +2,8 @@
 #   Simulate, train, and then predict both on train and on test 
 ##############################################################################################################################
 
+MT="lasso-base"
+MTS="LassoBase"
 B=1
 W=10
 N=1000
@@ -12,9 +14,9 @@ CL=10
 
 for H in 0.25 0.5 0.8 0.95
 do
-    ./rs-bann simulate-xy base ${M} ${B} ${N} ${W} ${D} ${H} --json-data --init-gamma-shape 3 --init-gamma-scale 1
-    cd Base_b${B}_wh${W}_ws${W}_d${D}_m${M}_n${N}_h${H}_k3_s1
-    ../rs-bann train base model.bin ${CL} ${IL} --trace
+    ./rs-bann simulate-xy ${MT} ${M} ${B} ${N} ${W} ${D} ${H} --json-data --init-gamma-shape 3 --init-gamma-scale 1
+    cd ${MTS}_b${B}_wh${W}_ws${W}_d${D}_m${M}_n${N}_h${H}_k3_s1
+    ../rs-bann train ${MT} model.bin ${CL} ${IL} --trace
     cd model_cl${CL}_il${IL}_Izmailov
     ../../rs-bann predict ../test.gen > test_pred.csv && ../../rs-bann predict ../train.gen > train_pred.csv 
     cd ../../
@@ -28,9 +30,9 @@ CL=100
 
 for H in 0.25 0.5 0.8 0.95
 do
-    # ./rs-bann simulate-xy base ${M} ${B} ${N} ${W} ${D} ${H} --json-data --init-gamma-shape 3 --init-gamma-scale 1
-    cd Base_b${B}_wh${W}_ws${W}_d${D}_m${M}_n${N}_h${H}_k3_s1
-    ../rs-bann train base model.bin ${CL} ${IL} --trace
+    # ./rs-bann simulate-xy ${MT} ${M} ${B} ${N} ${W} ${D} ${H} --json-data --init-gamma-shape 3 --init-gamma-scale 1
+    cd ${MTS}_b${B}_wh${W}_ws${W}_d${D}_m${M}_n${N}_h${H}_k3_s1
+    ../rs-bann train ${MT} model.bin ${CL} ${IL} --trace
     cd model_cl${CL}_il${IL}_Izmailov
     ../../rs-bann predict ../test.gen > test_pred.csv && ../../rs-bann predict ../train.gen > train_pred.csv 
     cd ../../
