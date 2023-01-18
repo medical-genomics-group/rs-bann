d1187c3a65606d2317e5f960b1c7fbadce056d12
nickmachnik <nick.machnik@gmail.com>
2023-01-18 11:42:54 +0100

    include top af_helpers

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,9 @@
 extern crate blas_src;
 extern crate openblas_src;
 
+pub mod af_helpers;
 pub mod data;
 pub mod error;
 pub mod group;
 pub mod linear_model;
 pub mod net;
-
-use arrayfire::Array;
-
-pub(crate) fn to_host(a: &Array<f32>) -> Vec<f32> {
-    let mut buffer = Vec::<f32>::new();
-    buffer.resize(a.elements(), 0.);
-    a.host(&mut buffer);
-    buffer
-}
-
-pub(crate) fn scalar_to_host(a: &Array<f32>) -> f32 {
-    let mut host_data = vec![0.0];
-    a.host(&mut host_data);
-    host_data[0]
-}
