b08021cd0c733787c94d289624aa1c8003e51c1e
nickmachnik <nick.machnik@gmail.com>
2023-02-07 11:09:00 +0100

    test bed vm

--- src/io/bed.rs
+++ src/io/bed.rs
@@ -98,7 +98,6 @@ impl BedVM {
         }
     }
 
-    /// Assumes VariantMajor format and no missing data.
     fn get_cols_af(&self, col_ixs: &[usize]) -> Vec<Array<f32>> {
         let mut res = Vec::new();
         for col_ix in col_ixs {
@@ -117,5 +116,84 @@ impl BedVM {
             ));
         }
         res
+    }
+
+    fn get_cols(&self, col_ixs: &[usize]) -> Vec<Vec<f32>> {
+        let mut res = Vec::new();
+        for col_ix in col_ixs {
+            let start_ix = col_ix * self.num_individuals;
+            let end_ix = start_ix + self.num_individuals;
+            let mut vals = Vec::with_capacity(self.num_bytes_per_col * 4);
+            let col_data = &self.data[start_ix..end_ix];
+            for b in col_data.iter() {
+                let lu_ix = 4 * (*b as usize);
+                vals.extend_from_slice(&BED_LOOKUP_GENOTYPE[lu_ix..lu_ix + 4]);
+            }
+            vals.truncate(self.num_individuals);
+            res.push(vals);
+        }
+        res
+    }
+
+    /// Decompresses data to f32.
+    /// Removes padding from columns.
+    fn data_f32(&self) -> Vec<f32> {
+        let mut res = Vec::with_capacity(self.num_individuals * self.num_markers);
+        for (ix, byte) in self.data.iter().enumerate() {
+            let lu_ix = 4 * (*byte as usize);
+            res.extend_from_slice(&BED_LOOKUP_GENOTYPE[lu_ix..lu_ix + 4]);
+            res.truncate(self.num_individuals * (ix + 1));
+        }
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use super::BedVM;
+
+    fn make_test_bed_vm() -> BedVM {
+        let base_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+        let base_path = Path::new(&base_dir);
+        let bed_path = base_path.join("resources/test/small");
+        BedVM::from_file(&bed_path)
+    }
+
+    #[test]
+    fn bed_vm_from_file() {
+        let bed_vm = make_test_bed_vm();
+        assert_eq!(bed_vm.num_individuals, 20);
+        assert_eq!(bed_vm.num_markers, 11);
+        let col_major_mat: Vec<f32> = vec![
+            0., 0., 1., 0., 1., 0., 0., 1., 0., 0., 1., 0., 0., 0., 0., 0., 1., 0., 2., 0., 1., 0.,
+            1., 0., 0., 2., 0., 0., 1., 1., 1., 1., 0., 0., 0., 1., 0., 0., 1., 0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 1.,
+            1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 1., 0., 0., 0., 0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 2., 0., 1., 1., 1., 2., 0., 1., 1.,
+            1., 1., 2., 0., 0., 1., 2., 1., 0., 1., 2., 0., 1., 0., 0., 0., 1., 0., 0., 0., 0., 1.,
+            1., 0., 0., 0., 0., 1., 1., 1., 1., 1., 0., 1., 1., 1., 1., 0., 1., 0., 1., 2., 2., 1.,
+            1., 1., 2., 1., 1., 1., 0., 0., 0., 0., 0., 2., 0., 0., 0., 0., 0., 1., 0., 0., 0., 2.,
+            0., 0., 0., 0., 0., 1., 0., 1., 1., 2., 0., 0., 0., 0., 1., 1., 1., 1., 1., 1., 1., 1.,
+            1., 0., 1., 1., 0., 1., 1., 0., 1., 0., 0., 2., 1., 1., 1., 1., 0., 0., 1., 1., 0., 0.,
+        ];
+        assert_eq!(bed_vm.data_f32(), col_major_mat);
+    }
+
+    fn bed_vm_get_cols() {
+        let bed_vm = make_test_bed_vm();
+        let cols = vec![0, 5];
+        let exp = [
+            [
+                0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, 0.0, 2.0, 0.0,
+            ],
+            [
+                0.0, 2.0, 0.0, 1.0, 1.0, 1.0, 2.0, 0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 0.0, 0.0, 1.0,
+                2.0, 1.0, 0.0, 1.0,
+            ],
+        ];
+        assert_eq!(bed_vm.get_cols(&cols), exp);
     }
 }
