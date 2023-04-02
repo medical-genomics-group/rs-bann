0748e60cfd8410dd30c35bd7d26305ffd5fa9eac
nickmachnik <nick.machnik@gmail.com>
2023-04-02 14:39:17 +0200

    add debug print for phen test

--- src/bin/rs-bann.rs
+++ src/bin/rs-bann.rs
@@ -909,6 +909,8 @@ fn load_ungrouped_data(
         ExternalGrouping::from_file(Path::new(&args.groups)),
     );
 
+    debug!("Trying to load test phen at: {:?}", args.p_train);
+
     let train_phen = Phenotypes::from_file(Path::new(&args.p_train))
         .expect("Failed to load train.phen training phenotypes");
 
